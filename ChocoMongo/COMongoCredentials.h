fd11d17c5a8a234bf80bfb20b81bada293df158a
Erik Aigner <erik@chocomoko.com>
2012-01-04 15:51:37 +0100

    db credentials header

---
+++ ChocoMongo/COMongoCredentials.h
@@ -0,0 +1,13 @@
+//
+//  COMongoCredentials.h
+//  ChocoMongo
+//
+//  Created by Erik Aigner on 04.01.12.
+//  Copyright (c) 2012 chocomoko.com. All rights reserved.
+//
+
+// ENTER YOUR MONGO DB CREDENTIALS HERE
+
+#define MONGO_HOST @"my.mongo.host"
+#define MONGO_PORT 29267
+#define MONGO_DB @"mydbname"
