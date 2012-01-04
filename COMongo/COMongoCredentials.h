a7479593f08baba5981fbf1a30c1a1ba0b1f5a18
Erik Aigner <erik@chocomoko.com>
2012-01-04 22:31:37 +0100

    localhost credentials file

---
+++ COMongo/COMongoCredentials.h
@@ -0,0 +1,15 @@
+//
+//  COMongoCredentials.h
+//  COMongo
+//
+//  Created by Erik Aigner on 04.01.12.
+//  Copyright (c) 2012 chocomoko.com. All rights reserved.
+//
+
+// ENTER YOUR MONGO DB CREDENTIALS HERE
+
+#define MONGO_HOST @"127.0.0.1"
+#define MONGO_PORT 27017
+#define MONGO_DB @"mydbname"
+#define MONGO_USER nil
+#define MONGO_PW nil
